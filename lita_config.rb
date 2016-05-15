b70f032ed7a0490d3d2225c14b81644747232d78
yumaito-mac <yumaito0@gmail.com>
2016-05-16 08:24:15 +0900

    どういたしまして

--- lita_config.rb
+++ lita_config.rb
@@ -9,7 +9,6 @@ Lita.configure do |config|
   # :debug, :info, :warn, :error, :fatal
   # Messages at the selected level and above will be logged.
   config.robot.log_level = :info
-
   # An array of user IDs that are considered administrators. These users
   # the ability to add and remove other users from authorization groups.
   # What is considered a user ID will change depending on which adapter you use.
