f86aee8f46fdb289f490a37c2efb06dc223801a7
yumaito-mac <yumaito0@gmail.com>
2016-05-15 22:23:46 +0900

    現在時刻

--- handlers/lita-reply_handler/lib/lita/handlers/reply_handler.rb
+++ handlers/lita-reply_handler/lib/lita/handlers/reply_handler.rb
@@ -5,11 +5,13 @@ module Lita
       @Day = %w(日 月 火 水 木 金 土)
       @Am_pm = { "AM" => "午前", "PM" => "午後" }
 
-      route /今何時？/, :reply_clock, help: {"今何時？" => "現在時刻を返してくれます"}
+      route /今何時？/, :reply_clock, help: {"brian2: 今何時？" => "現在時刻を返してくれます"}
 
       def reply_clock( response )
-          response.reply("知らん")
+          time = Time.now
+          response.reply(time.to_s)
       end
+
       Lita.register_handler(self)
     end
   end
