1ac2767a3c255ce815bf5e0119d323cf948f2c97
yumaito-mac <yumaito0@gmail.com>
2016-05-24 23:56:37 +0900

    google maps apiで日本語都市名を英語都市名に変換

--- handlers/lita-get_weather/lib/lita/handlers/get_weather.rb
+++ handlers/lita-get_weather/lib/lita/handlers/get_weather.rb
@@ -2,24 +2,39 @@ module Lita
   module Handlers
     class GetWeather < Handler
       # insert handler code here
-      @@API = ENV['OPEN_WEATHER_API_KEY']
+      @@Weather_API = ENV['OPEN_WEATHER_API_KEY']
+      @@Google_API = ENV['GOOGLE_MAP_API_KEY']
 
-
-      route /天気/, :weather, help: {"brian2: 天気" => "天気を教えてくれます"}
+      route /天気[\s|\p{blank}](.+)/, :weather, help: {"brian2: 天気　「都市名」" => "天気を教えてくれます。（スペースは半角でも全角でも可）"}
 
       def weather(response)
-        key = 'http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=b108411c925b3f19909bfb7bb3a7103e'
+        city_name = response.matches[0][0]
+
+        city_name_en = detect_city_name(city_name)
+
+        key = 'http://api.openweathermap.org/data/2.5/weather?q='+city_name_en+'&appid='+ @@Weather_API
         uri = URI.parse(key)
         data = http.get(uri)
         result  = MultiJson.load(data.body)
+        #
+
         # 表示に必要な情報を整形
-        name = result["name"]
         desc= result["weather"][0]["description"].gsub(/(\s)/,'_')
         desc_jp = t("weather_code.#{desc}")
         #
-        replyStr = "#{name}の天気は#{desc_jp}"
+        replyStr = "#{city_name}の天気は#{desc_jp}"
         response.reply(replyStr)
       end
+      #  日本語都市名から英語都市名に変換（google maps api経由） 
+      def detect_city_name(city_name)
+          address = 'https://maps.googleapis.com/maps/api/geocode/json?address='+city_name+'&key='+ @@Google_API
+          address_uri = URI.escape(address)
+          address_data = http.get(address_uri)
+          address_result = MultiJson.load(address_data.body)
+          city_name_en = address_result["results"][0]["address_components"][0]["long_name"]
+          return city_name_en
+      end
+
       Lita.register_handler(self)
     end
   end
