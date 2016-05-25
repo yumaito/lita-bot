7da03f320424f11e0d89174f7511143be28639fd
yumaito-mac <yumaito0@gmail.com>
2016-05-25 21:49:34 +0900

    地名が存在しないときの返事

--- handlers/lita-get_weather/lib/lita/handlers/get_weather.rb
+++ handlers/lita-get_weather/lib/lita/handlers/get_weather.rb
@@ -4,33 +4,41 @@ module Lita
       # insert handler code here
       @@Weather_API = ENV['OPEN_WEATHER_API_KEY']
       @@Google_API = ENV['GOOGLE_MAP_API_KEY']
+      @@no_result = "ZERO_RESULTS"
+      @@ok = "OK"
 
       route /天気[\s|\p{blank}](.+)/, :weather, help: {"brian2: 天気　「都市名」" => "天気を教えてくれます。（スペースは半角でも全角でも可）"}
 
       def weather(response)
         city_name = response.matches[0][0]
+        mention_name = response.user.metadata["mention_name"]
+        city = detect_city_name(city_name)
+        if (city == @@no_result)
+            response.reply("#{mention_name}: そんな地名ねーよ")
+        else
+            key = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ @@Weather_API
+            uri = URI.parse(key)
+            data = http.get(uri)
+            result  = MultiJson.load(data.body)
+            #
+            # 表示に必要な情報を整形
+            desc = result["weather"][0]["description"].gsub(/(\s)/,'_')
+            desc_jp = t("weather_code.#{desc}")
 
-        city_name_en = detect_city_name(city_name)
-
-        key = 'http://api.openweathermap.org/data/2.5/weather?q='+city_name_en+'&appid='+ @@Weather_API
-        uri = URI.parse(key)
-        data = http.get(uri)
-        result  = MultiJson.load(data.body)
-        #
-
-        # 表示に必要な情報を整形
-        desc= result["weather"][0]["description"].gsub(/(\s)/,'_')
-        desc_jp = t("weather_code.#{desc}")
-        #
-        replyStr = "#{city_name}の天気は#{desc_jp}"
-        response.reply(replyStr)
+            reply_str = "#{city_name}の天気は#{desc_jp}"
+            response.reply(reply_str)
+        end
       end
-      #  日本語都市名から英語都市名に変換（google maps api経由） 
+      #  日本語都市名から英語都市名に変換（google maps api経由）
       def detect_city_name(city_name)
-          address = 'https://maps.googleapis.com/maps/api/geocode/json?address='+city_name+'&key='+ @@Google_API
+          address = 'https://maps.googleapis.com/maps/api/geocode/json?address='+city_name+'&key='+ @@Google_API + '&language=en'
           address_uri = URI.escape(address)
           address_data = http.get(address_uri)
           address_result = MultiJson.load(address_data.body)
+          if (address_result["status"] == @@no_result)
+              return @@no_result
+          end
+          result = {}
           city_name_en = address_result["results"][0]["address_components"][0]["long_name"]
           return city_name_en
       end
